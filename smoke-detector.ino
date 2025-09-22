94bb0ea5e2bf1a0d92a3cf53fcf0f6fd228948f9
Chaiyaphan P. <105531232+RiiAEL@users.noreply.github.com>
2025-09-22 12:57:02 +0700

    Update smoke-detector.ino add build number for test

--- smoke-detector.ino
+++ smoke-detector.ino
@@ -1,8 +1,8 @@
-// MQ-2 smoke detector for Arduinoo nano board only
+// MQ-2 smoke detector for Arduino nano board only build 1
 const int sensorPin = A0;
 const int buzzerPin = 2;
 const int ledPin = 13;
-const int  relayPin = 3;
+const int relayPin = 3;
 
 int threshold = 300;
 
@@ -14,12 +14,12 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(relayPin, OUTPUT);
 
-  digitalWrite(buzzerPin, LOW)
+  digitalWrite(buzzerPin, LOW);
   digitalWrite(ledPin, LOW);
   digitalWrite(relayPin, LOW);
 
   Serial.begin(9600);
-  delay(200);
+  delay(2000);
   Serial.println("MQ-2 Smoke Detector Start");
   Serial.println("Please Allow");
 }
@@ -28,21 +28,22 @@ void loop() {
   int raw = analogRead(sensorPin);
 
   if (raw > threshold) {
-    digitalWrite(buzzerPin, HIGH)
+    digitalWrite(buzzerPin, HIGH);
     digitalWrite(ledPin, HIGH);
     digitalWrite(relayPin, HIGH);
   } else {
-    digitalWrite(buzzerPin, LOW)
+    digitalWrite(buzzerPin, LOW);
     digitalWrite(ledPin, LOW);
     digitalWrite(relayPin, LOW);
   }
 
   if (millis() - lastPrint > printInterval) {
     lastPrint = millis();
-    Serial.print("MQ-2 raw");
+    Serial.print("MQ-2 raw: ");
     Serial.print(raw);
-    Serial.print("threhold :");
+    Serial.print("  threshold: ");
     Serial.println(threshold);
   }
+
   delay(100);
 }

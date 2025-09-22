8fcd5b83faf46fd763fe3ae01f13624a6ba490ac
Chaiyaphan P. <105531232+RiiAEL@users.noreply.github.com>
2025-09-22 12:44:02 +0700

    Update and fix some code

--- smoke-detector.ino
+++ smoke-detector.ino
@@ -1,12 +1,12 @@
 // MQ-2 smoke detector for Arduinoo nano board only
-const int sensorPin = A0;     //MQ-2 A0
-const int buzzerPin = 2;      //activeBuzzer
-const int ledPin = 13;        //LED
-const relayPin = 3;           //relay
+const int sensorPin = A0;
+const int buzzerPin = 2;
+const int ledPin = 13;
+const int  relayPin = 3;
 
-int threshold = 300;          //standard setting - should calobrate
+int threshold = 300;
 
-unsigned long = lastPrint = 0;
+unsigned long lastPrint = 0;
 const unsigned long printInterval = 1000;
 
 void setup() {
@@ -14,30 +14,27 @@ void setup() {
   pinMode(ledPin, OUTPUT);
   pinMode(relayPin, OUTPUT);
 
-  digitalWrite(buzzerPin, LOW)
-  digitalWrite(ledPin, LOW);
-  digitalWrite(relayPin, LOW);
+  digitalWrtie(buzzerPin, LOW)
+  digitalWrtie(ledPin, LOW);
+  digitalWrtie(relayPin, LOW);
 
   Serial.begin(9600);
   delay(200);
-  Serial.println("MQ-2 Smoke Detector Start")
-  Serial.println("Please Allow")
+  Serial.println("MQ-2 Smoke Detector Start");
+  Serial.println("Please Allow");
 }
 
 void loop() {
-  int raw = analogRead(sensorPin);    //0-1023
-
-  //if you want to convert to pressure : float voltage raw * (5.0 / 1023.0);
+  int raw = analogRead(sensorPin);
 
   if (raw > threshold) {
-    //alarm
-    digitalWrite(buzzerPin, HIGH)
-    digitalWrite(ledPin, HIGH);
-    digitalWrite(relayPin, HIGH);
+    digitalWrtie(buzzerPin, HIGH)
+    digitalWrtie(ledPin, HIGH);
+    digitalWrtie(relayPin, HIGH);
   } else {
-    digitalWrite(buzzerPin, LOW)
-    digitalWrite(ledPin, LOW);
-    digitalWrite(relayPin, LOW);
+    digitalWrtie(buzzerPin, LOW)
+    digitalWrtie(ledPin, LOW);
+    digitalWrtie(relayPin, LOW);
   }
 
   if (millis() - lastPrint > printInterval) {
@@ -45,7 +42,7 @@ void loop() {
     Serial.print("MQ-2 raw");
     Serial.print(raw);
     Serial.print("threhold :");
-    Serial.println(threhold);
+    Serial.println(threshold);
   }
   delay(100);
 }
